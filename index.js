7af7cf2661ffe2789b2ee1ebf2a85eefbd53a42d
Asutosh Biswal <97107828+AsutoshBiswal007@users.noreply.github.com>
2024-01-20 11:43:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+    
+        var typed = new Typed('#element', {
+          strings: ['HTML','CSS','JAVASCRIPT','REACT JS','CORE JAVA','PYTHON','HTML,CSS,JAVASCRIPT,REACT JS,CORE JAVA,PYTHON etc.'],
+          typeSpeed: 50,
+        });
+      
